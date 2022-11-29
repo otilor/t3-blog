7dcf9ec0ff46097a7a638d071d27de9bfe47f42f
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-11-29 10:38:33 +0100

    add post

--- components/PostComponent.tsx
+++ components/PostComponent.tsx
@@ -1,4 +1,5 @@
 import { BoltIcon, DevicePhoneMobileIcon, GlobeAltIcon, ScaleIcon } from '@heroicons/react/24/outline'
+import { PrismaClient } from '@prisma/client';
 
 const features = [
   {
@@ -26,8 +27,10 @@ const features = [
     icon: DevicePhoneMobileIcon,
   },
 ]
+function Example(props: { foods: unknown; }) {
+  const foods = props.foods;
+  console.log(foods);
 
-export default function Example() {
   return (
     <div className="py-24 sm:py-32 lg:py-40">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -42,14 +45,14 @@ export default function Example() {
 
         <div className="mt-20 max-w-lg sm:mx-auto md:max-w-none">
           <div className="grid grid-cols-1 gap-y-16 md:grid-cols-2 md:gap-x-12 md:gap-y-16">
-            {features.map((feature) => (
-              <div key={feature.name} className="relative flex flex-col gap-6 sm:flex-row md:flex-col lg:flex-row">
+            {foods?.map((food, i) => (
+              <div key={i} className="relative flex flex-col gap-6 sm:flex-row md:flex-col lg:flex-row">
                 <div className="flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-500 text-white sm:shrink-0">
-                  <feature.icon className="h-8 w-8" aria-hidden="true" />
+                  {/* <feature.icon className="h-8 w-8" aria-hidden="true" /> */}
                 </div>
                 <div className="sm:min-w-0 sm:flex-1">
-                  <p className="text-lg font-semibold leading-8 text-gray-900">{feature.name}</p>
-                  <p className="mt-2 text-base leading-7 text-gray-600">{feature.description}</p>
+                  <p className="text-lg font-semibold leading-8 text-gray-900">{food.title}</p>
+                  <p className="mt-2 text-base leading-7 text-gray-600">{food.content}</p>
                 </div>
               </div>
             ))}
@@ -59,3 +62,14 @@ export default function Example() {
     </div>
   )
 }
+
+export async function getServerSideProps() {
+  const prisma = new PrismaClient();
+  const allFoods = await prisma?.post.findMany();
+  return {
+    props: {
+      foods: allFoods,
+    }
+  }
+}
+export default Example;
